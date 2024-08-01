dae76c0c0b123eb1998e2d9091ad42559343661c
AmruthaKrish <mulintiamrutha2000@gmail.com>
2024-08-01 19:32:51 -0400

    Update ALLOWED_HOSTS for Vercel deployment

--- django_delights/settings.py
+++ django_delights/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-vu-w=+fn1108r*^ss68hm3y_vlrsq$83-_al+wp9ph$5$1hdcd"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['djangodelights.vercel.app']
 
 
 # Application definition
@@ -120,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
