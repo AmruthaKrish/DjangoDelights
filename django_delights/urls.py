9a189b704f78225c495d0f8b808d378d9ecc6466
Amrutha Mulinti <93768016+AmruthaKrish@users.noreply.github.com>
2024-08-01 19:57:51 -0400

    Update urls.py

--- django_delights/urls.py
+++ django_delights/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from inventory import views as inventory_views
+from django.views.generic.base import RedirectView 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -25,4 +26,5 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(),name = 'login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('signup/', inventory_views.signup, name='signup'),
+    path('', RedirectView.as_view(url='/login/', permanent=True)),
 ]
